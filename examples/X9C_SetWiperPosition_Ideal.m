15c7f2dd82a8b5bf49025c177c4124b981039bd4
tingerlingerr <ray.upamanyu4pwork@gmail.com>
2025-05-18 16:09:55 +0530

    Update X9C_SetWiperPosition_Ideal.m

--- examples/X9C_SetWiperPosition_Ideal.m
+++ examples/X9C_SetWiperPosition_Ideal.m
@@ -2,12 +2,9 @@
 
 clear; close all; clc;
 
-% Create the object
-
-
 % Customize default properties
 model_name = 'X9C10C';   % name of simscape block
-digitpot_model = 104;      % 102 - 1kohm, 103 - 10kohm, 104 - 100kohm
+digitpot_model = 104;    % 102 - 1kohm, 103 - 10kohm, 104 - 100kohm
 sim_time = 3;  % seconds
 
 % Begin simulation
@@ -15,5 +12,5 @@ wiper_cmd = 46;
 
 pot = X9C(model_name, digitpot_model, sim_time);
 
-pot.setWiperPos_Ideal(wiper_cmd);
+pot.setWiperPos_Ideal(wiper_cmd);  % 
 disp("Finished.");
